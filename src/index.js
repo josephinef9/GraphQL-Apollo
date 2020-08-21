17b8ee120349552ae66c671d2dc1de93688f6f02
Jo Fischer <josephinefischer9@gmail.com>
2020-08-21 11:36:29 -0400

    add authentication

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom'
+import { AUTH_TOKEN } from './constants'
 // 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 // 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
