9758ff5510acbe19b23e14ca7790bd8798c75db4
Jo Fischer <josephinefischer9@gmail.com>
2020-08-21 13:44:20 -0400

    add subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,6 +3,27 @@ import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
 export const FEED_QUERY = gql`
 {
   feed {
@@ -26,7 +47,35 @@ export const FEED_QUERY = gql`
 }
 `
 
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
 class LinkList extends Component {
+
   _updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY })
 
@@ -35,12 +84,43 @@ class LinkList extends Component {
 
     store.writeQuery({ query: FEED_QUERY, data })
   }
+
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    })
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Fetching</div>
           if (error) return <div>Error</div>
+
+          this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
+
 
           const linksToRender = data.feed.links
 
